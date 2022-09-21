647f5640722f914a7494c205256f722a181719df
Sarah Strange <strange1120@gmail.com>
2022-09-21 14:43:31 -0400

    feat: starting to add candy core and candy guard

---
+++ nft_ingester/src/program_transformers/candy_machine_guard.rs/candy_guard.rs
@@ -0,0 +1,49 @@
+
+use crate::{
+    program_transformers::{candy_machine::state::CandyMachine, common::save_changelog_event},
+    IngesterError,
+};
+use blockbuster::{
+    instruction::InstructionBundle,
+    programs::bubblegum::{BubblegumInstruction, LeafSchema, Payload},
+};
+use candy_machine::state::CandyMachine;
+use digital_asset_types::{
+    adapter::{TokenStandard, UseMethod, Uses},
+    dao::candy_machine_collections,
+    json::ChainDataV1,
+};
+use num_traits::FromPrimitive;
+use plerkle_serialization::{
+    account_info_generated::account_info::AccountInfo,
+    transaction_info_generated::transaction_info::{self},
+};
+use sea_orm::{
+    entity::*, query::*, sea_query::OnConflict, ConnectionTrait, DatabaseTransaction, DbBackend,
+    EntityTrait, JsonValue,
+};
+
+pub async fn candy_guard<'c>(
+    candy_guard: &CandyGuard,
+    acct: &AccountInfo<'c>,
+    txn: &'c DatabaseTransaction,
+) -> Result<(), IngesterError> {
+    let (candy_guard, candy_guard_data) = candy_guard;
+
+    // let candy_guard = candy_machine_collections::ActiveModel {
+    //     mint: Set(collections.mint.to_bytes().to_vec()),
+    //     candy_machine: Set(collections.candy_machine.to_bytes().to_vec()),
+    //     ..Default::default()
+    // };
+
+    // let query = candy_machine_collections::Entity::insert(model)
+    //     .on_conflict(
+    //         OnConflict::columns([candy_machine_collections::Column::Id])
+    //             .do_nothing()
+    //             .to_owned(),
+    //     )
+    //     .build(DbBackend::Postgres);
+    // txn.execute(query).await.map(|_| ()).map_err(Into::into);
+
+    Ok(())
+}
