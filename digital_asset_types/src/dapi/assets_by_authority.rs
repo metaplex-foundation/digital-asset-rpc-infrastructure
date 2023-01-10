8eec081edfbf1a140b7a98acac542fc1c2d26028
austbot <me@austbot.com>
2023-01-10 07:33:04 -0600

    git is hard adding files

---
+++ digital_asset_types/src/dapi/assets_by_authority.rs
@@ -0,0 +1,30 @@
+use crate::dao::scopes;
+use crate::rpc::filter::AssetSorting;
+use crate::rpc::response::AssetList;
+use sea_orm::DatabaseConnection;
+use sea_orm::DbErr;
+
+use super::common::{build_asset_response, create_pagination, create_sorting};
+
+pub async fn get_assets_by_authority(
+    db: &DatabaseConnection,
+    authority: Vec<u8>,
+    sorting: AssetSorting,
+    limit: u64,
+    page: Option<u64>,
+    before: Option<Vec<u8>>,
+    after: Option<Vec<u8>>,
+) -> Result<AssetList, DbErr> {
+    let pagination = create_pagination(before, after, page)?;
+    let (sort_direction, sort_column) = create_sorting(sorting);
+    let assets = scopes::asset::get_by_authority(
+        db,
+        authority,
+        sort_column,
+        sort_direction,
+        &pagination,
+        limit,
+    )
+    .await?;
+    Ok(build_asset_response(assets, limit, &pagination))
+}
