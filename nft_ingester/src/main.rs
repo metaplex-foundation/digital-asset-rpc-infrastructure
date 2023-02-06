132d5101b07d1677e3c4075cdf31ae668f9bbdbf
austbot <me@austbot.com>
2023-02-06 01:08:28 -0600

    make txn shorter, add migration

--- nft_ingester/src/main.rs
+++ nft_ingester/src/main.rs
@@ -20,14 +20,21 @@ use plerkle_messenger::{
     redis_messenger::RedisMessenger, Messenger, MessengerConfig, RecvData, ACCOUNT_STREAM,
     TRANSACTION_STREAM,
 };
-use plerkle_serialization::{root_as_account_info, root_as_transaction_info, Pubkey as FBPubkey, solana_geyser_plugin_interface_shims::SlotStatus};
+use plerkle_serialization::{
+    root_as_account_info, root_as_transaction_info,
+    solana_geyser_plugin_interface_shims::SlotStatus, Pubkey as FBPubkey,
+};
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
 use serde::Deserialize;
-use std::{sync::Arc};
 use sqlx::{self, postgres::PgPoolOptions, Pool, Postgres};
 use std::fmt::{Display, Formatter};
 use std::net::UdpSocket;
-use tokio::{sync::{mpsc::UnboundedSender, Semaphore}, task::JoinSet, time};
+use std::sync::Arc;
+use tokio::{
+    sync::{mpsc::UnboundedSender, Semaphore},
+    task::JoinSet,
+    time,
+};
 
 // Types and constants used for Figment configuration items.
 pub type DatabaseConfig = figment::value::Dict;
@@ -148,7 +155,7 @@ async fn main() {
     let account_stream = service_account_stream::<RedisMessenger>(
         pool.clone(),
         backgroun_task_sender,
-        config.messenger_config.clone()
+        config.messenger_config.clone(),
     );
 
     let mut tasks = JoinSet::new();
@@ -268,7 +275,7 @@ async fn service_transaction_stream<T: Messenger>(
 async fn service_account_stream<T: Messenger>(
     pool: Pool<Postgres>,
     tasks: UnboundedSender<TaskData>,
-    messenger_config: MessengerConfig
+    messenger_config: MessengerConfig,
 ) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
         loop {
@@ -279,10 +286,9 @@ async fn service_account_stream<T: Messenger>(
                 let manager = Arc::new(ProgramTransformer::new(pool_cloned, tasks_cloned));
                 let mut messenger = T::new(messenger_config_cloned).await.unwrap();
                 println!("Setting up account listener");
-                let sem = Semaphore::new(100);
                 loop {
                     if let Ok(data) = messenger.recv(ACCOUNT_STREAM).await {
-                        let ids = handle_account(&sem, &manager, data).await;
+                        let ids = handle_account(&manager, data).await;
                         if !ids.is_empty() {
                             if let Err(e) = messenger.ack_msg(ACCOUNT_STREAM, &ids).await {
                                 println!("Error ACK-ing messages {:?}", e);
@@ -307,35 +313,26 @@ async fn service_account_stream<T: Messenger>(
     })
 }
 
-async fn handle_account(semaphore: &Semaphore, manager: &Arc<ProgramTransformer>, data: Vec<RecvData>) -> Vec<String> {
+async fn handle_account(
+    manager: &Arc<ProgramTransformer>,
+    data: Vec<RecvData>,
+) -> Vec<String> {
     safe_metric(|| {
         statsd_gauge!("ingester.account_batch_size", data.len() as u64);
     });
-
-    let tasks = FuturesUnordered::new();
+    let mut ids = Vec::new();
     for item in data.into_iter() {
         let manager = Arc::clone(manager);
+        let id = item.id;
+        if item.tries > 0 {
+            safe_metric(|| {
+                statsd_count!("ingester.account_stream_redelivery", 1);
+            });
+        }
 
-        tasks.push(async move {
-            let id = item.id;
-            let mut ids = Vec::new();
-            let p = semaphore.acquire().await;
-            if p.is_err() {
-                safe_metric(|| {
-                    statsd_count!("ingester.concurrency_error", 1);
-                });
-                return ids;
-            }
-            if item.tries > 0 {
-                safe_metric(|| {
-                    statsd_count!("ingester.account_stream_redelivery", 1);
-                });
-            }
-            
-            let data = item.data;
-            // Get root of account info flatbuffers object.
-           if let Ok(account_update) = root_as_account_info(&data) {
-             
+        let data = item.data;
+        // Get root of account info flatbuffers object.
+        if let Ok(account_update) = root_as_account_info(&data) {
             let seen_at = Utc::now();
             let str_program_id =
                 bs58::encode(account_update.owner().unwrap().0.as_slice()).into_string();
@@ -379,18 +376,10 @@ async fn handle_account(semaphore: &Semaphore, manager: &Arc<ProgramTransformer>
                         statsd_count!("ingester.account_update_error", 1, "owner" => &str_program_id);
                     });
                 }
-            
             }
         }
-            ids
-        });
     }
-    tasks
-        .collect::<Vec<_>>()
-        .await
-        .into_iter()
-        .flatten()
-        .collect()
+    ids
 }
 
 async fn process_instruction<'i>(
