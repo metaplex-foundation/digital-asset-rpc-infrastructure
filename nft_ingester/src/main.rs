5b3f1c87c50e332438443d3257dbb36b43bbeabc
austbot <me@austbot.com>
2023-02-06 02:16:58 -0600

    back to async

--- nft_ingester/src/main.rs
+++ nft_ingester/src/main.rs
@@ -20,21 +20,14 @@ use plerkle_messenger::{
     redis_messenger::RedisMessenger, Messenger, MessengerConfig, RecvData, ACCOUNT_STREAM,
     TRANSACTION_STREAM,
 };
-use plerkle_serialization::{
-    root_as_account_info, root_as_transaction_info,
-    solana_geyser_plugin_interface_shims::SlotStatus, Pubkey as FBPubkey,
-};
+use plerkle_serialization::{root_as_account_info, root_as_transaction_info, Pubkey as FBPubkey, solana_geyser_plugin_interface_shims::SlotStatus};
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
 use serde::Deserialize;
+use std::{sync::Arc};
 use sqlx::{self, postgres::PgPoolOptions, Pool, Postgres};
 use std::fmt::{Display, Formatter};
 use std::net::UdpSocket;
-use std::sync::Arc;
-use tokio::{
-    sync::{mpsc::UnboundedSender, Semaphore},
-    task::JoinSet,
-    time,
-};
+use tokio::{sync::{mpsc::UnboundedSender, Semaphore}, task::JoinSet, time};
 
 // Types and constants used for Figment configuration items.
 pub type DatabaseConfig = figment::value::Dict;
@@ -155,7 +148,7 @@ async fn main() {
     let account_stream = service_account_stream::<RedisMessenger>(
         pool.clone(),
         backgroun_task_sender,
-        config.messenger_config.clone(),
+        config.messenger_config.clone()
     );
 
     let mut tasks = JoinSet::new();
@@ -275,7 +268,7 @@ async fn service_transaction_stream<T: Messenger>(
 async fn service_account_stream<T: Messenger>(
     pool: Pool<Postgres>,
     tasks: UnboundedSender<TaskData>,
-    messenger_config: MessengerConfig,
+    messenger_config: MessengerConfig
 ) -> tokio::task::JoinHandle<()> {
     tokio::spawn(async move {
         loop {
@@ -313,26 +306,28 @@ async fn service_account_stream<T: Messenger>(
     })
 }
 
-async fn handle_account(
-    manager: &Arc<ProgramTransformer>,
-    data: Vec<RecvData>,
-) -> Vec<String> {
+async fn handle_account(manager: &Arc<ProgramTransformer>, data: Vec<RecvData>) -> Vec<String> {
     safe_metric(|| {
         statsd_gauge!("ingester.account_batch_size", data.len() as u64);
     });
-    let mut ids = Vec::new();
+
+    let tasks = FuturesUnordered::new();
     for item in data.into_iter() {
         let manager = Arc::clone(manager);
-        let id = item.id;
-        if item.tries > 0 {
-            safe_metric(|| {
-                statsd_count!("ingester.account_stream_redelivery", 1);
-            });
-        }
 
-        let data = item.data;
-        // Get root of account info flatbuffers object.
-        if let Ok(account_update) = root_as_account_info(&data) {
+        tasks.push(async move {
+            let id = item.id;
+            let mut ids = Vec::new();
+            if item.tries > 0 {
+                safe_metric(|| {
+                    statsd_count!("ingester.account_stream_redelivery", 1);
+                });
+            }
+            
+            let data = item.data;
+            // Get root of account info flatbuffers object.
+           if let Ok(account_update) = root_as_account_info(&data) {
+             
             let seen_at = Utc::now();
             let str_program_id =
                 bs58::encode(account_update.owner().unwrap().0.as_slice()).into_string();
@@ -376,10 +371,18 @@ async fn handle_account(
                         statsd_count!("ingester.account_update_error", 1, "owner" => &str_program_id);
                     });
                 }
+            
             }
         }
+            ids
+        });
     }
-    ids
+    tasks
+        .collect::<Vec<_>>()
+        .await
+        .into_iter()
+        .flatten()
+        .collect()
 }
 
 async fn process_instruction<'i>(
