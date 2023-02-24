c05dcd2cc8eb1d7ce91abcc5c265b059a62d137e
austbot <me@austbot.com>
2023-02-24 15:45:39 -0600

    fixes migration for real

--- migration/src/m20230224_093722_performance_improvements.rs
+++ migration/src/m20230224_093722_performance_improvements.rs
@@ -40,21 +40,97 @@ impl MigrationTrait for Migration {
                 DatabaseBackend::Postgres,
                 "
                 DROP INDEX IF EXISTS ta_delegate;
-                ALTER TABLE asset DROP CONSTRAINT IF EXISTS asset_asset_data_fkey;
+                "
+                .to_string(),
+            ))
+            .await?;
+
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
                 DROP INDEX IF EXISTS asset_revision;
+                "
+                .to_string(),
+            ))
+            .await?;
+
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
+                ALTER TABLE asset DROP CONSTRAINT IF EXISTS asset_asset_data_fkey;
+                "
+                .to_string(),
+            ))
+            .await?;
+
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
                 DROP INDEX IF EXISTS asset_tree_leaf;
+                "
+                .to_string(),
+            ))
+            .await?;
 
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
                 ALTER TABLE asset_authority DROP CONSTRAINT IF EXISTS asset_authority_asset_id_fkey;
-                ALTER TABLE asset_creators DROP CONSTRAINT IF EXISTS asset_creators_asset_id_fkey;
-                ALTER TABLE asset_grouping DROP CONSTRAINT IF EXISTS asset_grouping_asset_id_fkey;
-                ALTER TABLE asset_v1_account_attachments DROP CONSTRAINT IF EXISTS asset_v1_account_attachments_asset_id_fkey;
+                "
+                .to_string(),
+            ))
+            .await?;
 
-                DROP INDEX IF EXISTS asset_grouping_asset_id;
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
+                ALTER TABLE asset_creators DROP CONSTRAINT IF EXISTS asset_creators_asset_id_fkey;
+                "
+                .to_string(),
+            ))
+            .await?;
+
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
+                ALTER TABLE asset_grouping DROP CONSTRAINT IF EXISTS asset_grouping_asset_id_fkey;
+                "
+                .to_string(),
+            ))
+            .await?;
+
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
+                ALTER TABLE asset_v1_account_attachments DROP CONSTRAINT IF EXISTS asset_v1_account_attachments_asset_id_fkey;
                 ".to_string(),
             ))
             .await?;
 
-            
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
+                DROP INDEX IF EXISTS asset_grouping_asset_id;
+                "
+                .to_string(),
+            ))
+            .await?;
 
         Ok(())
     }
