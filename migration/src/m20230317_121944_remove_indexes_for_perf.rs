ef6679660d816d598d2108bfce9dccce8603c9bc
austbot <me@austbot.com>
2023-03-17 15:54:16 -0500

    remove indexs

---
+++ migration/src/m20230317_121944_remove_indexes_for_perf.rs
@@ -0,0 +1,79 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
+                DROP INDEX IF EXISTS backfill_items_failed_idx;
+                "
+                .to_string(),
+            ))
+            .await?;
+
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
+                DROP INDEX IF EXISTS backfill_items_locked_idx;
+                "
+                .to_string(),
+            ))
+            .await?;
+
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
+                DROP INDEX IF EXISTS cl_items_tree_idx;
+                "
+                .to_string(),
+            ))
+            .await?;
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
+                DROP INDEX IF EXISTS backfill_items_slot_idx;;
+                "
+                .to_string(),
+            ))
+            .await?;
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
+                DROP INDEX IF EXISTS backfill_items_force_chk_idx;;
+                "
+                .to_string(),
+            ))
+            .await?;
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "
+                DROP INDEX IF EXISTS backfill_items_backfilled_idx;;
+                "
+                .to_string(),
+            ))
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
