4d0379093524e5300ed4b080594369b16e439b01
linuskendall <git@linuskendall.com>
2023-04-20 16:44:12 +0100

    added basic version of bgtask creator

---
+++ nft_ingester/src/lib.rs
@@ -0,0 +1,11 @@
+pub mod account_updates;
+pub mod ack;
+pub mod backfiller;
+pub mod config;
+pub mod database;
+pub mod error;
+pub mod metrics;
+pub mod stream;
+pub mod tasks;
+pub mod transaction_notifications;
+pub mod program_transformers;
