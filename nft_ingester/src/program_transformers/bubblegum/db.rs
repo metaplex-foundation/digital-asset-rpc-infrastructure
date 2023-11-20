7e3f9ef84327471666e48ae931ae621a5cb10ea9
Kirill Fomichev <fanatid@ya.ru>
2023-11-20 13:50:36 -0500

    add global clippy lints

--- nft_ingester/src/program_transformers/bubblegum/db.rs
+++ nft_ingester/src/program_transformers/bubblegum/db.rs
@@ -23,7 +23,7 @@ where
     Ok(change_log_event.seq)
 }
 
-fn node_idx_to_leaf_idx(index: i64, tree_height: u32) -> i64 {
+const fn node_idx_to_leaf_idx(index: i64, tree_height: u32) -> i64 {
     index - 2i64.pow(tree_height)
 }
 
