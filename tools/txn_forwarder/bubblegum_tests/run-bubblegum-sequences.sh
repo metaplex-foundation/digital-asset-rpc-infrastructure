43475a8ad72bee98774e48b2424a1c8905f8c16c
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-01-03 13:20:56 -0800

    Require asset and cl_items files to exist

--- tools/txn_forwarder/bubblegum_tests/run-bubblegum-sequences.sh
+++ tools/txn_forwarder/bubblegum_tests/run-bubblegum-sequences.sh
@@ -10,18 +10,6 @@ SCENARIOS=("mint_transfer_burn.scenario" \
 "mint_transfer_transfer.scenario" \
 "mint_delegate_transfer.scenario"
 )
-EXPECTED_ASSET_VALUES=("mint_transfer_burn_asset.txt" \
-"mint_redeem_decompress_asset.txt" \
-"mint_redeem_cancel_redeem_redeem_decompress_asset.txt" \
-"mint_transfer_transfer_asset.txt" \
-"mint_delegate_transfer_asset.txt"
-)
-EXPECTED_CL_ITEMS_VALUES=("mint_transfer_burn_cl_items.txt" \
-"mint_redeem_decompress_cl_items.txt" \
-"mint_redeem_cancel_redeem_redeem_decompress_cl_items.txt" \
-"mint_transfer_transfer_cl_items.txt" \
-"mint_delegate_transfer_cl_items.txt"
-)
 
 if [ ${#SCENARIOS[@]} -gt 0 ]; then
     echo "Running ${#SCENARIOS[@]} scenarios"
@@ -35,47 +23,60 @@ STATUS=0
 
 # Run each scenario and check for expected database result.
 for i in ${!SCENARIOS[@]}; do
+    # Read in the `asset` file for this scenario.
+    EXPECTED_ASSET_FILE="$(basename "${SCENARIOS[$i]}" .scenario)_asset.txt"
+    if [ ! -f "$EXPECTED_ASSET_FILE" ]; then
+        echo $(RED "${SCENARIOS[$i]} missing asset file")
+        STATUS=1
+        continue
+    fi
+    EXPECTED_ASSET_VALUE=$(<"$EXPECTED_ASSET_FILE")
+
+    # Parse out the asset ID.
+    ASSET_ID=$(echo "$EXPECTED_ASSET_VALUE" | grep -oP '^(?!tree_id).*id\s+\|\s+\K[^ ]+')
+    if [ ${#ASSET_ID} -ne 66 ]; then
+        echo $(RED "${SCENARIOS[$i]} incorrect asset ID parsing")
+        echo "Asset ID: $ASSET_ID"
+        STATUS=1
+        continue
+    fi
 
     # Initially this asset should not be in `asset`` table.
-    if [ -f "${EXPECTED_ASSET_VALUES[$i]}" ]; then
-        EXPECTED_ASSET_VALUE=$(<"${EXPECTED_ASSET_VALUES[$i]}")
-        ASSET_ID=$(echo "$EXPECTED_ASSET_VALUE" | grep -oP '^(?!tree_id).*id\s+\|\s+\K[^ ]+')
-        if [ ${#ASSET_ID} -ne 66 ]; then
-            echo $(RED "${SCENARIOS[$i]} incorrect asset ID parsing")
-            STATUS=1
-            continue
-        fi
+    SQL="select * from asset where id = '$ASSET_ID';"
+    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$SQL")
+    if [ "(0 rows)" == "$DATABASE_VAL" ]; then
+        echo $(GRN "${SCENARIOS[$i]} initial asset table passed")
+    else
+        echo $(RED "${SCENARIOS[$i]} initial asset table failed")
+        STATUS=1
+    fi
 
-        SQL="select * from asset where id = '$ASSET_ID';"
-        DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$SQL")
-        
-        if [ "(0 rows)" == "$DATABASE_VAL" ]; then
-            echo $(GRN "${SCENARIOS[$i]} initial asset table passed")
-        else
-            echo $(RED "${SCENARIOS[$i]} initial asset table failed")
-            STATUS=1
-        fi
+    # Read in the `cl_items` file for this scenario.
+    EXPECTED_CL_ITEMS_FILE="$(basename "${SCENARIOS[$i]}" .scenario)_cl_items.txt"
+    if [ ! -f "$EXPECTED_CL_ITEMS_FILE" ]; then
+        echo $(RED "${SCENARIOS[$i]} missing cl_items file")
+        STATUS=1
+        continue
+    fi
+    EXPECTED_CL_ITEMS=$(<"$EXPECTED_CL_ITEMS_FILE")
+
+    # Parse out the tree ID.
+    TREE_ID=$(echo "$EXPECTED_CL_ITEMS" | grep -oP '^\s*\K\\x[0-9a-f]+' | head -n 1)
+    if [ ${#TREE_ID} -ne 66 ]; then
+        echo $(RED "${SCENARIOS[$i]} incorrect asset ID parsing")
+        echo "Tree ID: $TREE_ID"
+        STATUS=1
+        continue
     fi
 
     # Initially this tree should not be in `cl_items`` table.
-    if [ -f "${EXPECTED_CL_ITEMS_VALUES[$i]}" ]; then
-        EXPECTED_CL_ITEMS=$(<"${EXPECTED_CL_ITEMS_VALUES[$i]}")
-        TREE_ID=$(echo "$EXPECTED_CL_ITEMS" | grep -oP '^\s*\K\\x[0-9a-f]+' | head -n 1)
-        if [ ${#TREE_ID} -ne 66 ]; then
-            echo $(RED "${SCENARIOS[$i]} incorrect asset ID parsing")
-            STATUS=1
-            continue
-        fi
-
-        SQL="select tree, node_idx, leaf_idx, seq, level, hash from cl_items where tree = '$TREE_ID' order by level;"
-        DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$SQL")
-        
-        if [ "(0 rows)" == "$DATABASE_VAL" ]; then
-            echo $(GRN "${SCENARIOS[$i]} initial cl_items table passed")
-        else
-            echo $(RED "${SCENARIOS[$i]} initial cl_items table failed")
-            STATUS=1
-        fi
+    SQL="select tree, node_idx, leaf_idx, seq, level, hash from cl_items where tree = '$TREE_ID' order by level;"
+    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$SQL")
+    if [ "(0 rows)" == "$DATABASE_VAL" ]; then
+        echo $(GRN "${SCENARIOS[$i]} initial cl_items table passed")
+    else
+        echo $(RED "${SCENARIOS[$i]} initial cl_items table failed")
+        STATUS=1
     fi
 
     # Run the scenario file that indexes the asset.  These are done with separate calls to the `txn_forwarder`
@@ -95,41 +96,35 @@ for i in ${!SCENARIOS[@]}; do
     sleep 3
 
     # Asset should now be in `asset`` table and all fields except `created_at` date match.
-    if [ -f "${EXPECTED_ASSET_VALUES[$i]}" ]; then
-        SQL="select * from asset where id = '$ASSET_ID';"
-        DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$SQL")
-        DATABASE_VAL_NO_DATE=$(sed '/^created_at/d' <<< "$DATABASE_VAL")
-
-        if [ "$EXPECTED_ASSET_VALUE" == "$DATABASE_VAL_NO_DATE" ]; then
-            echo $(GRN "${SCENARIOS[$i]} asset table passed")
-        else
-            echo $(RED "${SCENARIOS[$i]} asset table failed")
-            echo "Asset ID: $ASSET_ID"
-            echo "Expected:"
-            echo "$EXPECTED_ASSET_VALUE"
-            echo "Actual:"
-            echo "$DATABASE_VAL_NO_DATE"
-            STATUS=1
-        fi
+    SQL="select * from asset where id = '$ASSET_ID';"
+    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$SQL")
+    DATABASE_VAL_NO_DATE=$(sed '/^created_at/d' <<< "$DATABASE_VAL")
+    if [ "$EXPECTED_ASSET_VALUE" == "$DATABASE_VAL_NO_DATE" ]; then
+        echo $(GRN "${SCENARIOS[$i]} asset table passed")
+    else
+        echo $(RED "${SCENARIOS[$i]} asset table failed")
+        echo "Asset ID: $ASSET_ID"
+        echo "Expected:"
+        echo "$EXPECTED_ASSET_VALUE"
+        echo "Actual:"
+        echo "$DATABASE_VAL_NO_DATE"
+        STATUS=1
     fi
 
     # Tree should now be in `cl_items`` table and all fields match.
-    if [ -f "${EXPECTED_CL_ITEMS_VALUES[$i]}" ]; then
-        SQL="select tree, node_idx, leaf_idx, seq, level, hash from cl_items where tree = '$TREE_ID' order by level;"
-        DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana --command="$SQL")
+    SQL="select tree, node_idx, leaf_idx, seq, level, hash from cl_items where tree = '$TREE_ID' order by level;"
+    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana --command="$SQL")
+    if [ "$EXPECTED_CL_ITEMS" == "$DATABASE_VAL" ]; then
+        echo $(GRN "${SCENARIOS[$i]} cl_items table passed")
+    else
+        echo $(RED "${SCENARIOS[$i]} cl_items table failed")
+        echo "Tree ID: $TREE_ID"
+        echo "Expected:"
+        echo "$EXPECTED_CL_ITEMS"
+        echo "Actual:"
+        echo "$DATABASE_VAL"
 
-        if [ "$EXPECTED_CL_ITEMS" == "$DATABASE_VAL" ]; then
-            echo $(GRN "${SCENARIOS[$i]} cl_items table passed")
-        else
-            echo $(RED "${SCENARIOS[$i]} cl_items table failed")
-            echo "Tree ID: $TREE_ID"
-            echo "Expected:"
-            echo "$EXPECTED_CL_ITEMS"
-            echo "Actual:"
-            echo "$DATABASE_VAL"
-
-            STATUS=1
-        fi
+        STATUS=1
     fi
 
     echo ""
