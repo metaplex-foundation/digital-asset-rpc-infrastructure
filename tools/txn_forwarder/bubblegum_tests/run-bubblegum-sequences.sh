922fcf1e43c72d4260b111cd4410e7f79a04de99
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-01-03 17:00:45 -0800

    Move repeated code to functions

--- tools/txn_forwarder/bubblegum_tests/run-bubblegum-sequences.sh
+++ tools/txn_forwarder/bubblegum_tests/run-bubblegum-sequences.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Output colors.
-RED() { echo $'\e[1;31m'$1$'\e[0m'; }
-GRN() { echo $'\e[1;32m'$1$'\e[0m'; }
-
 SCENARIOS=("mint_transfer_burn.scenario" \
 "mint_redeem_decompress.scenario"
 "mint_redeem_cancel_redeem_redeem_decompress.scenario" \
@@ -16,24 +12,63 @@ SCENARIOS=("mint_transfer_burn.scenario" \
 "mint_to_collection_unverify_collection.scenario"
 )
 
+TEST_SCENARIO_DATA_DIR="test_scenario_data"
+
+# Output text in colors.
+# $1 is output text.
+RED() { echo $'\e[1;31m'$1$'\e[0m'; }
+GRN() { echo $'\e[1;32m'$1$'\e[0m'; }
+
+# Read from database using psql.
+# $1 is SQL command.
+# $2 extra CLI args to send to psql.
+# $3 is expected value.
+# $4 is topic for the pass/fail message.
+READ_FROM_DATABASE() {
+    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana "$2" --command="$1")
+    DATABASE_VAL=$(sed '/^created_at/d' <<< "$DATABASE_VAL")
+    if [ "$3" = "$DATABASE_VAL" ]; then
+        echo $(GRN "${SCENARIOS[$i]} $4 passed") >&2
+        return 0
+    else
+        echo $(RED "${SCENARIOS[$i]} $4 failed") >&2
+        echo "Asset ID: $ASSET_ID" >&2
+        echo "Expected:" >&2
+        echo "$3" >&2
+        echo "Actual:" >&2
+        echo "$DATABASE_VAL" >&2
+        return 1
+    fi
+}
+
+# Read in expected data from test data file.
+# $1 is scenario file to use as a base name.
+# $2 is the suffix for the type of test data, i.e. "asset", "cl_items", etc.
+READ_IN_EXPECTED_DATA() {
+    local EXPECTED_DATA_FILE="$TEST_SCENARIO_DATA_DIR/$(basename "$1" .scenario)_"$2".txt"
+    if [ -f "$EXPECTED_DATA_FILE" ]; then
+        cat "$EXPECTED_DATA_FILE"
+        return 0
+    else
+        echo $(RED "$1 missing $2 file") >&2
+        return 1
+    fi
+}
+
 if [ ${#SCENARIOS[@]} -gt 0 ]; then
     echo "Running ${#SCENARIOS[@]} scenarios"
 fi
-TEST_SCENARIO_DATA_DIR="test_scenario_data"
 
 # 0 is pass, 1 is fail.
 STATUS=0
 
 # Run each scenario and check for expected database result.
 for i in ${!SCENARIOS[@]}; do
-    # Read in the `asset` file for this scenario.
-    EXPECTED_ASSET_FILE="$TEST_SCENARIO_DATA_DIR/$(basename "${SCENARIOS[$i]}" .scenario)_asset.txt"
-    if [ ! -f "$EXPECTED_ASSET_FILE" ]; then
-        echo $(RED "${SCENARIOS[$i]} missing asset file")
-        STATUS=1
-        continue
-    fi
-    EXPECTED_ASSET_VALUE=$(<"$EXPECTED_ASSET_FILE")
+    # Read in the expected database data for this scenario.
+    EXPECTED_ASSET_VALUE=$(READ_IN_EXPECTED_DATA "${SCENARIOS[$i]}" "asset") || { STATUS=1; continue; }
+    EXPECTED_ASSET_CREATORS=$(READ_IN_EXPECTED_DATA "${SCENARIOS[$i]}" "asset_creators") || { STATUS=1; continue; }
+    EXPECTED_ASSET_GROUPING=$(READ_IN_EXPECTED_DATA "${SCENARIOS[$i]}" "asset_grouping") || { STATUS=1; continue; }
+    EXPECTED_CL_ITEMS=$(READ_IN_EXPECTED_DATA "${SCENARIOS[$i]}" "cl_items") || { STATUS=1; continue; }
 
     # Parse out the asset ID.
     ASSET_ID=$(echo "$EXPECTED_ASSET_VALUE" | grep -oP '^(?!tree_id).*id\s+\|\s+\K[^ ]+')
@@ -44,68 +79,6 @@ for i in ${!SCENARIOS[@]}; do
         continue
     fi
 
-    # Initially this asset should not be in `asset`` table.
-    ASSET_SQL="SELECT * FROM asset WHERE id = '$ASSET_ID';"
-    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$ASSET_SQL")
-    if [ "(0 rows)" == "$DATABASE_VAL" ]; then
-        echo $(GRN "${SCENARIOS[$i]} initial asset table passed")
-    else
-        echo $(RED "${SCENARIOS[$i]} initial asset table failed")
-        STATUS=1
-    fi
-
-    # Read in the `asset_creators` file for this scenario.
-    EXPECTED_ASSET_CREATORS_FILE="$TEST_SCENARIO_DATA_DIR/$(basename "${SCENARIOS[$i]}" .scenario)_asset_creators.txt"
-    if [ ! -f "$EXPECTED_ASSET_CREATORS_FILE" ]; then
-        echo $(RED "${SCENARIOS[$i]} missing asset_creators file")
-        STATUS=1
-        continue
-    fi
-    EXPECTED_ASSET_CREATORS=$(<"$EXPECTED_ASSET_CREATORS_FILE")
-
-    # Initially this asset should not be in `asset_creators`` table.
-    ASSET_CREATORS_SQL="SELECT asset_id, creator, share, verified, seq, slot_updated, position \
-        FROM asset_creators \
-        WHERE asset_id = '$ASSET_ID' \
-        ORDER BY position;"
-    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$ASSET_CREATORS_SQL")
-    if [ "(0 rows)" == "$DATABASE_VAL" ]; then
-        echo $(GRN "${SCENARIOS[$i]} initial asset_creators table passed")
-    else
-        echo $(RED "${SCENARIOS[$i]} initial asset_creators table failed")
-        STATUS=1
-    fi
-
-    # Read in the `asset_grouping` file for this scenario.
-    EXPECTED_ASSET_GROUPING_FILE="$TEST_SCENARIO_DATA_DIR/$(basename "${SCENARIOS[$i]}" .scenario)_asset_grouping.txt"
-    if [ ! -f "$EXPECTED_ASSET_GROUPING_FILE" ]; then
-        echo $(RED "${SCENARIOS[$i]} missing asset_grouping file")
-        STATUS=1
-        continue
-    fi
-    EXPECTED_ASSET_GROUPING=$(<"$EXPECTED_ASSET_GROUPING_FILE")
-
-    # Initially this asset should not be in `asset_grouping`` table.
-    ASSET_GROUPING_SQL="SELECT asset_id, group_key, group_value, seq, slot_updated, verified, group_info_seq \
-        FROM asset_grouping \
-        WHERE asset_id = '$ASSET_ID';"
-    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$ASSET_GROUPING_SQL")
-    if [ "(0 rows)" == "$DATABASE_VAL" ]; then
-        echo $(GRN "${SCENARIOS[$i]} initial asset_grouping table passed")
-    else
-        echo $(RED "${SCENARIOS[$i]} initial asset_grouping table failed")
-        STATUS=1
-    fi
-
-    # Read in the `cl_items` file for this scenario.
-    EXPECTED_CL_ITEMS_FILE="$TEST_SCENARIO_DATA_DIR/$(basename "${SCENARIOS[$i]}" .scenario)_cl_items.txt"
-    if [ ! -f "$EXPECTED_CL_ITEMS_FILE" ]; then
-        echo $(RED "${SCENARIOS[$i]} missing cl_items file")
-        STATUS=1
-        continue
-    fi
-    EXPECTED_CL_ITEMS=$(<"$EXPECTED_CL_ITEMS_FILE")
-
     # Parse out the tree ID.
     TREE_ID=$(echo "$EXPECTED_CL_ITEMS" | grep -oP '^\s*\K\\x[0-9a-f]+' | head -n 1)
     if [ ${#TREE_ID} -ne 66 ]; then
@@ -115,15 +88,23 @@ for i in ${!SCENARIOS[@]}; do
         continue
     fi
 
-    # Initially this tree should not be in `cl_items`` table.
-    SQL="select tree, node_idx, leaf_idx, seq, level, hash from cl_items where tree = '$TREE_ID' order by level;"
-    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$SQL")
-    if [ "(0 rows)" == "$DATABASE_VAL" ]; then
-        echo $(GRN "${SCENARIOS[$i]} initial cl_items table passed")
-    else
-        echo $(RED "${SCENARIOS[$i]} initial cl_items table failed")
-        STATUS=1
-    fi
+    # Initially this asset should not be in any database tables.
+    ASSET_SQL="SELECT * FROM asset WHERE id = '$ASSET_ID';"
+    READ_FROM_DATABASE "$ASSET_SQL" "-x" "(0 rows)" "initial asset table state" || STATUS=1
+
+    ASSET_CREATORS_SQL="SELECT asset_id, creator, share, verified, seq, slot_updated, position \
+        FROM asset_creators \
+        WHERE asset_id = '$ASSET_ID' \
+        ORDER BY position;"
+    READ_FROM_DATABASE "$ASSET_CREATORS_SQL" "-x" "(0 rows)" "initial asset_creators table state" || STATUS=1
+
+    ASSET_GROUPING_SQL="SELECT asset_id, group_key, group_value, seq, slot_updated, verified, group_info_seq \
+        FROM asset_grouping \
+        WHERE asset_id = '$ASSET_ID';"
+    READ_FROM_DATABASE "$ASSET_GROUPING_SQL" "-x" "(0 rows)" "initial asset_grouping table state" || STATUS=1
+
+    CL_ITEMS_SQL="select tree, node_idx, leaf_idx, seq, level, hash from cl_items where tree = '$TREE_ID' order by level;"
+    READ_FROM_DATABASE "$CL_ITEMS_SQL" "-x" "(0 rows)" "initial cl_items table state" || STATUS=1
 
     # Run the scenario file that indexes the asset.  These are done with separate calls to the `txn_forwarder`
     # in order to enforce order.  Just calling the `txn_forwarder` with the file results in random ordering.
@@ -141,64 +122,11 @@ for i in ${!SCENARIOS[@]}; do
 
     sleep 2
 
-    # Asset should now be in `asset` table and all fields except `created_at` date match.
-    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$ASSET_SQL")
-    DATABASE_VAL_NO_DATE=$(sed '/^created_at/d' <<< "$DATABASE_VAL")
-    if [ "$EXPECTED_ASSET_VALUE" == "$DATABASE_VAL_NO_DATE" ]; then
-        echo $(GRN "${SCENARIOS[$i]} asset table passed")
-    else
-        echo $(RED "${SCENARIOS[$i]} asset table failed")
-        echo "Asset ID: $ASSET_ID"
-        echo "Expected:"
-        echo "$EXPECTED_ASSET_VALUE"
-        echo "Actual:"
-        echo "$DATABASE_VAL_NO_DATE"
-        STATUS=1
-    fi
-
-    # Asset should now be in `asset_creators` table and all fields match.
-    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$ASSET_CREATORS_SQL")
-    if [ "$EXPECTED_ASSET_CREATORS" == "$DATABASE_VAL" ]; then
-        echo $(GRN "${SCENARIOS[$i]} asset_creators table passed")
-    else
-        echo $(RED "${SCENARIOS[$i]} asset_creators table failed")
-        echo "Asset ID: $ASSET_ID"
-        echo "Expected:"
-        echo "$EXPECTED_ASSET_CREATORS"
-        echo "Actual:"
-        echo "$DATABASE_VAL"
-        STATUS=1
-    fi
-
-    # Asset should now be in `asset_grouping` table and all fields match.
-    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana -x --command="$ASSET_GROUPING_SQL")
-    if [ "$EXPECTED_ASSET_GROUPING" == "$DATABASE_VAL" ]; then
-        echo $(GRN "${SCENARIOS[$i]} asset_grouping table passed")
-    else
-        echo $(RED "${SCENARIOS[$i]} asset_grouping table failed")
-        echo "Asset ID: $ASSET_ID"
-        echo "Expected:"
-        echo "$EXPECTED_ASSET_GROUPING"
-        echo "Actual:"
-        echo "$DATABASE_VAL"
-        STATUS=1
-    fi
-
-    # Tree should now be in `cl_items`` table and all fields match.
-    SQL="select tree, node_idx, leaf_idx, seq, level, hash from cl_items where tree = '$TREE_ID' order by level;"
-    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana --command="$SQL")
-    if [ "$EXPECTED_CL_ITEMS" == "$DATABASE_VAL" ]; then
-        echo $(GRN "${SCENARIOS[$i]} cl_items table passed")
-    else
-        echo $(RED "${SCENARIOS[$i]} cl_items table failed")
-        echo "Tree ID: $TREE_ID"
-        echo "Expected:"
-        echo "$EXPECTED_CL_ITEMS"
-        echo "Actual:"
-        echo "$DATABASE_VAL"
-
-        STATUS=1
-    fi
+    # Asset should now be in the database and all fields match (except `created_at` in `asset`` table).
+    READ_FROM_DATABASE "$ASSET_SQL" "-x" "$EXPECTED_ASSET_VALUE" "asset table" || STATUS=1
+    READ_FROM_DATABASE "$ASSET_CREATORS_SQL" "-x" "$EXPECTED_ASSET_CREATORS" "asset_creators table" || STATUS=1
+    READ_FROM_DATABASE "$ASSET_GROUPING_SQL" "-x" "$EXPECTED_ASSET_GROUPING" "asset_grouping table" || STATUS=1
+    READ_FROM_DATABASE "$CL_ITEMS_SQL" "" "$EXPECTED_CL_ITEMS" "cl_items table" || STATUS=1
 
     echo ""
 done
