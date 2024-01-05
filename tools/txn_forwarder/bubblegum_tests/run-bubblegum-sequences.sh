259fe4a4492513b898b2c252fa0d49ac4d6832f3
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-01-04 23:50:26 -0800

    Add support for running sequences in reverse

--- tools/txn_forwarder/bubblegum_tests/run-bubblegum-sequences.sh
+++ tools/txn_forwarder/bubblegum_tests/run-bubblegum-sequences.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Pass `reverse` to run scenarios in reverse.
+if [ "$1" = "reverse" ]; then
+    REVERSE="true"
+else
+    REVERSE="false"
+fi
+
 SCENARIOS=("mint_transfer_burn.scenario" \
 "mint_redeem_decompress.scenario"
 "mint_redeem_cancel_redeem_redeem_decompress.scenario" \
@@ -19,14 +26,16 @@ TEST_SCENARIO_DATA_DIR="test_scenario_data"
 RED() { echo $'\e[1;31m'$1$'\e[0m'; }
 GRN() { echo $'\e[1;32m'$1$'\e[0m'; }
 
-# Read from database using psql.
+# Read from database using psql and compare to expected value.
 # $1 is SQL command.
 # $2 extra CLI args to send to psql.
 # $3 is expected value.
 # $4 is topic for the pass/fail message.
-READ_FROM_DATABASE() {
-    DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana "$2" --command="$1")
-    DATABASE_VAL=$(sed '/^created_at/d' <<< "$DATABASE_VAL")
+# Returns 0 if database value matches expected value, otherwise returns 1.
+CHECK_DATABASE() {
+    local DATABASE_VAL=$(PGPASSWORD=solana psql -h localhost -U solana "$2" --command="$1")
+    # Remove `created_at` since the date changes for `asset` table entries.
+    local DATABASE_VAL=$(sed '/^created_at/d' <<< "$DATABASE_VAL")
     if [ "$3" = "$DATABASE_VAL" ]; then
         echo $(GRN "${SCENARIOS[$i]} $4 passed") >&2
         return 0
@@ -35,17 +44,30 @@ READ_FROM_DATABASE() {
         echo "Asset ID: $ASSET_ID" >&2
         echo "Expected:" >&2
         echo "$3" >&2
+        echo ""
         echo "Actual:" >&2
         echo "$DATABASE_VAL" >&2
         return 1
     fi
 }
 
-# Read in expected data from test data file.
+# Read in expected data from test data file.  If the $REVERSE flag is set to "true" then first
+# look for a file with the `_reverse` suffix. If one does not exist, use the default filename
+# for that suffix.
 # $1 is scenario file to use as a base name.
 # $2 is the suffix for the type of test data, i.e. "asset", "cl_items", etc.
+# If successful, prints contents of file on stdout and returns 0, otherwise returns 1.
 READ_IN_EXPECTED_DATA() {
-    local EXPECTED_DATA_FILE="$TEST_SCENARIO_DATA_DIR/$(basename "$1" .scenario)_"$2".txt"
+    local BASE_NAME=$(basename "$1" .scenario)
+    if [ "$REVERSE" = "true" ]; then
+        local EXPECTED_DATA_FILE_BW="$TEST_SCENARIO_DATA_DIR/${BASE_NAME}_"$2"_reverse.txt"
+        if [ -f "$EXPECTED_DATA_FILE_BW" ]; then
+            cat "$EXPECTED_DATA_FILE_BW"
+            return 0
+        fi
+    fi
+
+    local EXPECTED_DATA_FILE="$TEST_SCENARIO_DATA_DIR/${BASE_NAME}_"$2".txt"
     if [ -f "$EXPECTED_DATA_FILE" ]; then
         cat "$EXPECTED_DATA_FILE"
         return 0
@@ -55,8 +77,10 @@ READ_IN_EXPECTED_DATA() {
     fi
 }
 
-if [ ${#SCENARIOS[@]} -gt 0 ]; then
-    echo "Running ${#SCENARIOS[@]} scenarios"
+if [ "$REVERSE" = "true" ]; then
+    echo "Running ${#SCENARIOS[@]} scenarios in reverse"
+else
+    echo "Running ${#SCENARIOS[@]} scenarios forwards"
 fi
 
 # 0 is pass, 1 is fail.
@@ -90,25 +114,34 @@ for i in ${!SCENARIOS[@]}; do
 
     # Initially this asset should not be in any database tables.
     ASSET_SQL="SELECT * FROM asset WHERE id = '$ASSET_ID';"
-    READ_FROM_DATABASE "$ASSET_SQL" "-x" "(0 rows)" "initial asset table state" || STATUS=1
+    CHECK_DATABASE "$ASSET_SQL" "-x" "(0 rows)" "initial asset table state" || STATUS=1
 
     ASSET_CREATORS_SQL="SELECT asset_id, creator, share, verified, seq, slot_updated, position \
         FROM asset_creators \
         WHERE asset_id = '$ASSET_ID' \
         ORDER BY position;"
-    READ_FROM_DATABASE "$ASSET_CREATORS_SQL" "-x" "(0 rows)" "initial asset_creators table state" || STATUS=1
+    CHECK_DATABASE "$ASSET_CREATORS_SQL" "-x" "(0 rows)" "initial asset_creators table state" || STATUS=1
 
     ASSET_GROUPING_SQL="SELECT asset_id, group_key, group_value, seq, slot_updated, verified, group_info_seq \
         FROM asset_grouping \
         WHERE asset_id = '$ASSET_ID';"
-    READ_FROM_DATABASE "$ASSET_GROUPING_SQL" "-x" "(0 rows)" "initial asset_grouping table state" || STATUS=1
+    CHECK_DATABASE "$ASSET_GROUPING_SQL" "-x" "(0 rows)" "initial asset_grouping table state" || STATUS=1
 
     CL_ITEMS_SQL="select tree, node_idx, leaf_idx, seq, level, hash from cl_items where tree = '$TREE_ID' order by level;"
-    READ_FROM_DATABASE "$CL_ITEMS_SQL" "-x" "(0 rows)" "initial cl_items table state" || STATUS=1
+    CHECK_DATABASE "$CL_ITEMS_SQL" "-x" "(0 rows)" "initial cl_items table state" || STATUS=1
 
     # Run the scenario file that indexes the asset.  These are done with separate calls to the `txn_forwarder`
     # in order to enforce order.  Just calling the `txn_forwarder` with the file results in random ordering.
     readarray -t TXS < "$TEST_SCENARIO_DATA_DIR/${SCENARIOS[$i]}"
+
+    # Reverse transactions if necessary.
+    if [ "$REVERSE" = "true" ]; then
+        REVERSED_TXS=()
+        for ((j = ${#TXS[@]} - 1; j >= 0; j--)); do
+            REVERSED_TXS+=("${TXS[j]}")
+        done
+        TXS=("${REVERSED_TXS[@]}")
+    fi
 
     for TX in ${TXS[@]}; do
         (cd .. && \
@@ -123,19 +156,24 @@ for i in ${!SCENARIOS[@]}; do
     sleep 2
 
     # Asset should now be in the database and all fields match (except `created_at` in `asset`` table).
-    READ_FROM_DATABASE "$ASSET_SQL" "-x" "$EXPECTED_ASSET_VALUE" "asset table" || STATUS=1
-    READ_FROM_DATABASE "$ASSET_CREATORS_SQL" "-x" "$EXPECTED_ASSET_CREATORS" "asset_creators table" || STATUS=1
-    READ_FROM_DATABASE "$ASSET_GROUPING_SQL" "-x" "$EXPECTED_ASSET_GROUPING" "asset_grouping table" || STATUS=1
-    READ_FROM_DATABASE "$CL_ITEMS_SQL" "" "$EXPECTED_CL_ITEMS" "cl_items table" || STATUS=1
+    CHECK_DATABASE "$ASSET_SQL" "-x" "$EXPECTED_ASSET_VALUE" "asset table" || STATUS=1
+    CHECK_DATABASE "$ASSET_CREATORS_SQL" "-x" "$EXPECTED_ASSET_CREATORS" "asset_creators table" || STATUS=1
+    CHECK_DATABASE "$ASSET_GROUPING_SQL" "-x" "$EXPECTED_ASSET_GROUPING" "asset_grouping table" || STATUS=1
+    CHECK_DATABASE "$CL_ITEMS_SQL" "" "$EXPECTED_CL_ITEMS" "cl_items table" || STATUS=1
 
     echo ""
 done
 
-echo ""
-if [ $STATUS -eq 1 ]; then
-    echo $(RED "SOME TESTS FAILED!")
+if [ "$REVERSE" = "true" ]; then
+    SUFFIX="IN REVERSE"
 else
-    echo $(GRN "ALL TESTS PASSED!")
+    SUFFIX="FORWARDS"
+fi
+
+if [ $STATUS -eq 1 ]; then
+    echo $(RED "SOME TESTS FAILED $SUFFIX!")
+else
+    echo $(GRN "ALL TESTS PASSED $SUFFIX!")
 fi
 
 exit $STATUS
