2b4d2dcde6003f71d5f93f0d18dfdb20ba06c45e
Nicolas Pennie <nicolas.pennie@gmail.com>
2024-01-24 11:28:48 -0800

    pr comments

--- nft_ingester/src/program_transformers/bubblegum/db.rs
+++ nft_ingester/src/program_transformers/bubblegum/db.rs
@@ -532,46 +532,60 @@ pub async fn upsert_asset_creators<T>(
 where
     T: ConnectionTrait + TransactionTrait,
 {
-    let db_creators = creators
-        .iter()
-        .enumerate()
-        .map(|(i, creator)| asset_creators::ActiveModel {
+    let db_creators = if creators.is_empty() {
+        // If creators are empty, insert an empty creator with the current sequence.
+        // This prevents accidental errors during out-of-order updates.
+        vec![asset_creators::ActiveModel {
             asset_id: Set(id.clone()),
-            position: Set(i as i16),
-            creator: Set(creator.address.to_bytes().to_vec()),
-            share: Set(creator.share as i32),
-            verified: Set(creator.verified),
+            position: Set(0),
+            creator: Set(vec![]),
+            share: Set(100),
+            verified: Set(false),
             slot_updated: Set(Some(slot_updated)),
             seq: Set(Some(seq)),
             ..Default::default()
-        })
-        .collect::<Vec<_>>();
+        }]
+    } else {
+        creators
+            .iter()
+            .enumerate()
+            .map(|(i, c)| asset_creators::ActiveModel {
+                asset_id: Set(id.clone()),
+                position: Set(i as i16),
+                creator: Set(c.address.to_bytes().to_vec()),
+                share: Set(c.share as i32),
+                verified: Set(c.verified),
+                slot_updated: Set(Some(slot_updated)),
+                seq: Set(Some(seq)),
+                ..Default::default()
+            })
+            .collect()
+    };
 
-    if !db_creators.is_empty() {
-        // This statement will update base information for each creator.
-        let mut query = asset_creators::Entity::insert_many(db_creators)
-            .on_conflict(
-                OnConflict::columns([
-                    asset_creators::Column::AssetId,
-                    asset_creators::Column::Position,
-                ])
-                .update_columns([
-                    asset_creators::Column::Creator,
-                    asset_creators::Column::Share,
-                    asset_creators::Column::Verified,
-                    asset_creators::Column::Seq,
-                    asset_creators::Column::SlotUpdated,
-                ])
-                .to_owned(),
-            )
-            .build(DbBackend::Postgres);
+    // This statement will update base information for each creator.
+    let mut query = asset_creators::Entity::insert_many(db_creators)
+        .on_conflict(
+            OnConflict::columns([
+                asset_creators::Column::AssetId,
+                asset_creators::Column::Position,
+            ])
+            .update_columns([
+                asset_creators::Column::Creator,
+                asset_creators::Column::Share,
+                asset_creators::Column::Verified,
+                asset_creators::Column::Seq,
+                asset_creators::Column::SlotUpdated,
+            ])
+            .to_owned(),
+        )
+        .build(DbBackend::Postgres);
 
-        query.sql = format!(
+    query.sql = format!(
         "{} WHERE (asset_creators.seq != 0 AND excluded.seq >= asset_creators.seq) OR asset_creators.seq IS NULL",
         query.sql
     );
-        txn.execute(query).await?;
-    }
+
+    txn.execute(query).await?;
 
     Ok(())
 }
