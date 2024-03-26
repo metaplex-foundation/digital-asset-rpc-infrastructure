88df0d7611fc29a1b4ae480d404d515a315313a3
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-03-25 19:00:51 -0700

    add integ test for mpl-core getAssetsByGroup

--- integration_tests/tests/integration_tests/mpl_core_tests.rs
+++ integration_tests/tests/integration_tests/mpl_core_tests.rs
@@ -65,3 +65,44 @@ async fn test_mpl_core_get_asset() {
     let response = setup.das_api.get_asset(request).await.unwrap();
     insta::assert_json_snapshot!(name, response);
 }
+
+#[tokio::test]
+#[serial]
+#[named]
+async fn test_mpl_core_get_assets_by_group() {
+    let name = trim_test_name(function_name!());
+    let setup = TestSetup::new_with_options(
+        name.clone(),
+        TestSetupOptions {
+            network: Some(Network::Devnet),
+        },
+    )
+    .await;
+
+    let seeds: Vec<SeedEvent> = seed_accounts([
+        "6Wp9xk6GrD4EmDKuD7fr2URJubjtGq5MXENR9UU15C9i",
+        "3YhJuW9X9Hvf4MVv5qP5xWxEMPrQu8uTwjkKR1q7D1gh",
+        "79Npv5WTGGkfVc4QYhmnz9xRUbCPV4g5aNKvotvGo4Ko",
+        "9qS8Xo1M3RUqvrLwd5RaA6iMbzh9An7nwDSDe1aB5mtr",
+    ]);
+
+    apply_migrations_and_delete_data(setup.db.clone()).await;
+    index_seed_events(&setup, seeds.iter().collect_vec()).await;
+
+    let request = r#"
+    {
+        "groupKey": "collection",
+        "groupValue": "6Wp9xk6GrD4EmDKuD7fr2URJubjtGq5MXENR9UU15C9i",
+        "sortBy": {
+            "sortBy": "updated",
+            "sortDirection": "asc"
+        },
+        "page": 1,
+        "limit": 50
+    }
+    "#;
+
+    let request: api::GetAssetsByGroup = serde_json::from_str(request).unwrap();
+    let response = setup.das_api.get_assets_by_group(request).await.unwrap();
+    insta::assert_json_snapshot!(name, response);
+}
