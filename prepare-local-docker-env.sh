546e6ebc949331e9ff20930d85fe1120d56990dd
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2024-10-23 22:35:56 -0700

    Refactor and update docker prepare script

--- prepare-local-docker-env.sh
+++ prepare-local-docker-env.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 # go to parent folder
 cd $(dirname $(dirname $SCRIPT_DIR))
 
-EXTERNAL_ID=("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s" \
+EXTERNAL_ID_MAINNET=("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s" \
 "BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY" \
 "cmtDvXumGCrqC1Age74AVPhSRVXJMd8PJS91L8KbNCK" \
 "noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV" \
@@ -17,54 +17,71 @@ EXTERNAL_ID=("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s" \
 "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA" \
 "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb" \
 )
-EXTERNAL_SO=("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s.so" \
-"BGUMAp9Gq7iTEuizy4pqaxsTyUCBK68MDfK752saRPUY.so" \
-"cmtDvXumGCrqC1Age74AVPhSRVXJMd8PJS91L8KbNCK.so" \
-"noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV.so"
-"ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL.so" \
-"TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA.so" \
-"TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb.so" \
+
+EXTERNAL_ID_DEVNET=("mcmt6YrQEMKw8Mw43FmpRLmf7BqRnFMKmAcbxE3xkAW" \
+"mnoopTCrg4p8ry25e4bcWA9XZjbNjMTfgYVGGEdRsf3" \
 )
 
-if [ -z ${RPC+x} ]; then
-    RPC="https://api.mainnet-beta.solana.com"
-fi
+RPC_MAINNET="https://api.mainnet-beta.solana.com"
+RPC_DEVNET="https://api.devnet.solana.com"
+OUTPUT=$CURRENT_DIR/programs
 
-if [ -z "$OUTPUT" ]; then
-    OUTPUT=$CURRENT_DIR/programs
-fi
+# check for command existence
+command -v solana >/dev/null 2>&1 || { echo $(RED "[ ERROR ] solana CLI not found"); exit 1; }
+command -v sha256sum >/dev/null 2>&1 || { echo $(RED "[ ERROR ] sha256sum not found"); exit 1; }
 
 # creates the output directory if it doesn't exist
-if [ ! -d ${OUTPUT} ]; then
-    mkdir ${OUTPUT}
-fi
+mkdir -p ${OUTPUT}
 
-# only prints this if we have external programs
-if [ ${#EXTERNAL_ID[@]} -gt 0 ]; then
-    echo "Dumping external programs to: '${OUTPUT}'"
-fi
+dump_programs() {
+    local RPC_URL=$1
+    shift
+    local IDS=("$@")
 
-# dump external programs binaries if needed
-for i in ${!EXTERNAL_ID[@]}; do
-    if [ ! -f "${OUTPUT}/${EXTERNAL_SO[$i]}" ]; then
-        solana program dump -u $RPC ${EXTERNAL_ID[$i]} ${OUTPUT}/${EXTERNAL_SO[$i]}
-    else
-        solana program dump -u $RPC ${EXTERNAL_ID[$i]} ${OUTPUT}/onchain-${EXTERNAL_SO[$i]} > /dev/null
-        ON_CHAIN=`sha256sum -b ${OUTPUT}/onchain-${EXTERNAL_SO[$i]} | cut -d ' ' -f 1`
-        LOCAL=`sha256sum -b ${OUTPUT}/${EXTERNAL_SO[$i]} | cut -d ' ' -f 1`
+    for i in "${!IDS[@]}"; do
+        local ID=${IDS[$i]}
+        local SO_FILE="${ID}.so"
+        local ONCHAIN_SO_FILE="onchain-${SO_FILE}"
 
-        if [ "$ON_CHAIN" != "$LOCAL" ]; then
-            echo $(RED "[ WARNING ] on-chain and local binaries are different for '${EXTERNAL_SO[$i]}'")
+        if [ ! -f "${OUTPUT}/${SO_FILE}" ]; then
+            solana program dump -u "$RPC_URL" "$ID" "${OUTPUT}/${SO_FILE}" || {
+                echo $(RED "[  ERROR  ] Failed to dump program '${SO_FILE}'")
+                exit 1
+            }
         else
-            echo "$(GRN "[ SKIPPED ]") on-chain and local binaries are the same for '${EXTERNAL_SO[$i]}'"
-        fi
+            solana program dump -u "$RPC_URL" "$ID" "${OUTPUT}/${ONCHAIN_SO_FILE}" > /dev/null || {
+                echo $(RED "[  ERROR  ] Failed to dump program '${SO_FILE}'")
+                exit 1
+            }
 
-        rm ${OUTPUT}/onchain-${EXTERNAL_SO[$i]}
-    fi
-done
+            ON_CHAIN=$(sha256sum -b "${OUTPUT}/${ONCHAIN_SO_FILE}" | cut -d ' ' -f 1)
+            LOCAL=$(sha256sum -b "${OUTPUT}/${SO_FILE}" | cut -d ' ' -f 1)
+
+            if [[ "$ON_CHAIN" != "$LOCAL" ]]; then
+                echo $(RED "[ WARNING ] on-chain and local binaries are different for '${SO_FILE}'")
+            else
+                echo "$(GRN "[ SKIPPED ]") on-chain and local binaries are the same for '${SO_FILE}'"
+            fi
+
+
+            rm "${OUTPUT}/${ONCHAIN_SO_FILE}"
+        fi
+    done
+}
+
+if [ ${#EXTERNAL_ID_MAINNET[@]} -gt 0 ]; then
+    echo "Dumping external accounts from mainnet to '${OUTPUT}':"
+    dump_programs "$RPC_MAINNET" "${EXTERNAL_ID_MAINNET[@]}"
+fi
+
+if [ ${#EXTERNAL_ID_DEVNET[@]} -gt 0 ]; then
+    echo ""
+    echo "Dumping external accounts from devnet to '${OUTPUT}':"
+    dump_programs "$RPC_DEVNET" "${EXTERNAL_ID_DEVNET[@]}"
+fi
 
 # only prints this if we have external programs
-if [ ${#EXTERNAL_ID[@]} -gt 0 ]; then
+if [ ${#EXTERNAL_ID_MAINNET[@]} -gt 0 ] || [ ${#EXTERNAL_ID_DEVNET[@]} -gt 0 ]; then
     echo ""
 fi
 
