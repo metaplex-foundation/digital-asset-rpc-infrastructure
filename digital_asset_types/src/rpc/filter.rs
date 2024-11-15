e57fd011aeea0f8b658de05ae17de0f3b726b76c
AhzamAkhtar <ahzam8186@gmail.com>
2024-11-16 00:46:14 +0530

    Search asset token type filter added

--- digital_asset_types/src/rpc/filter.rs
+++ digital_asset_types/src/rpc/filter.rs
@@ -1,5 +1,6 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use sea_orm::entity::prelude::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
@@ -29,6 +30,36 @@ pub enum AssetSortBy {
     RecentAction,
     #[serde(rename = "none")]
     None,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq ,EnumIter, DeriveActiveEnum, Serialize, Deserialize,JsonSchema)]
+#[sea_orm(
+    rs_type = "String",
+    db_type = "Enum",
+    enum_name = "specification_asset_class"
+)]
+pub enum TokenTypeClass {
+    #[sea_orm(string_value = "FUNGIBLE_ASSET")]
+    FungibleAsset,
+    #[sea_orm(string_value = "FUNGIBLE_TOKEN")]
+    FungibleToken,
+    // #[sea_orm(ignore)]
+    #[sea_orm(string_value = "NON_FUNGIBLE_ASSET")]
+    NonFungibleAsset,
+    #[sea_orm(string_value = "MPL_CORE_ASSET")]
+    MplCoreAsset,
+    #[sea_orm(string_value = "MPL_CORE_COLLECTION")]
+    MplCoreCollection,
+    #[sea_orm(string_value = "NFT")]
+    Nft,
+    #[sea_orm(string_value = "PROGRAMMABLE_NFT")]
+    ProgrammableNft,
+    // #[sea_orm(ignore)]
+    #[sea_orm(string_value = "COMPRESSED_NFT")]
+    CompressedNft,
+    // #[sea_orm(ignore)]
+    #[sea_orm(string_value = "ALL")]
+    All,
 }
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize, JsonSchema)]
