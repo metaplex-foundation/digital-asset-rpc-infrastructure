45f72196b6b7d8716e4a84475b5fe55c6be60efc
AhzamAkhtar <ahzam8186@gmail.com>
2024-11-30 04:41:38 +0530

    Filter duplicate asset id filters
    Users can pass in duplicate asset ids when calling getAssets. This
    remove the duplicates before processing the request.
    
    Co-authored-by: Kyle Espinola <kyle.s.espinola@gmail.com>

--- das_api/src/api/api_impl.rs
+++ das_api/src/api/api_impl.rs
@@ -218,6 +218,10 @@ impl ApiContract for DasApi {
     ) -> Result<Vec<Option<Asset>>, DasApiError> {
         let GetAssets { ids, options } = payload;
 
+        let mut ids: Vec<String> = ids.into_iter().collect();
+        ids.sort();
+        ids.dedup();
+
         let batch_size = ids.len();
         if batch_size > 1000 {
             return Err(DasApiError::BatchSizeExceededError);
--- integration_tests/tests/integration_tests/main.rs
+++ integration_tests/tests/integration_tests/main.rs
@@ -7,5 +7,6 @@ mod mpl_core_tests;
 mod nft_editions_tests;
 mod regular_nft_tests;
 mod show_inscription_flag_tests;
+mod test_get_assets_with_multiple_same_ids;
 mod test_show_zero_balance_filter;
 mod token_accounts_tests;
