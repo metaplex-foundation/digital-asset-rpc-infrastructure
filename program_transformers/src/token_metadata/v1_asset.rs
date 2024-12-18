0d8084432447feb89fe32d8a1b92f6837a7535c9
Kyle Espinola <kyle.s.espinola@gmail.com>
2024-12-18 10:19:45 -0500

    patch condition check on asset_grouping (#188)

--- program_transformers/src/token_metadata/v1_asset.rs
+++ program_transformers/src/token_metadata/v1_asset.rs
@@ -343,15 +343,17 @@ pub async fn save_v1_asset<T: ConnectionTrait + TransactionTrait>(
                 .action_cond_where(
                     Condition::all()
                         .add(
-                            Condition::any().add(
-                                Expr::tbl(
-                                    Alias::new("excluded"),
-                                    asset_grouping::Column::GroupValue,
+                            Condition::any()
+                                .add(
+                                    Expr::tbl(
+                                        Alias::new("excluded"),
+                                        asset_grouping::Column::GroupValue,
+                                    )
+                                    .ne(Expr::tbl(
+                                        asset_grouping::Entity,
+                                        asset_grouping::Column::GroupValue,
+                                    )),
                                 )
-                                .ne(Expr::tbl(
-                                    asset_grouping::Entity,
-                                    asset_grouping::Column::GroupValue,
-                                ))
                                 .add(
                                     Expr::tbl(
                                         Alias::new("excluded"),
@@ -362,7 +364,6 @@ pub async fn save_v1_asset<T: ConnectionTrait + TransactionTrait>(
                                         asset_grouping::Column::Verified,
                                     )),
                                 ),
-                            ),
                         )
                         .add(
                             Expr::tbl(asset_grouping::Entity, asset_grouping::Column::SlotUpdated)
