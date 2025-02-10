d7dd0d004c9d0f9e262e1bf554e28ae184635737
Nagaprasadvr <nagaprasadvr246@gmail.com>
2025-02-10 18:36:37 +0530

    chore: remove bubblegum

--- bubblegum/src/backfill/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod gap;
-pub mod worker;
--- bubblegum/src/backfill/worker/mod.rs
+++
@@ -1,9 +0,0 @@
-mod gap;
-mod program_transformer;
-mod transaction;
-mod tree;
-
-pub use gap::GapWorkerArgs;
-pub use program_transformer::ProgramTransformerWorkerArgs;
-pub use transaction::SignatureWorkerArgs;
-pub use tree::TreeWorkerArgs;
