de7acc70f6aeb7db2fdd5ef33eb77ced301f86d9
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2025-07-01 23:49:21 -0700

    Default to updating all assets if collection not found

--- program_transformers/src/mpl_core_program/v1_asset.rs
+++ program_transformers/src/mpl_core_program/v1_asset.rs
@@ -104,8 +104,8 @@ pub async fn save_v1_asset<T: ConnectionTrait + TransactionTrait>(
         UpdateAuthority::None => Pubkey::default().to_bytes().to_vec(),
     };
 
-    // If we are looking at a collection, see if there's an existing record and if so check
-    // if the authority changed.
+    // If this is a collection, check if there's an existing authority record.
+    // If found, determine whether the authority has changed.
     let collection_authority_changed = match account_data {
         MplCoreAccountData::Collection(_) => {
             let existing_record = asset_authority::Entity::find()
@@ -114,9 +114,15 @@ pub async fn save_v1_asset<T: ConnectionTrait + TransactionTrait>(
                 .await
                 .map_err(|db_err| ProgramTransformerError::AssetIndexError(db_err.to_string()))?;
 
+            // We need to update all assets in the collection if:
+            // 1. The collection is found and its authority has changed.
+            // 2. The collection is not found.
+            //
+            // Case 2 requires us to attempt to update all assets in a collection because assets
+            // can be indexed before the collection itself.
             existing_record
                 .map(|record| record.authority != update_authority)
-                .unwrap_or(false)
+                .unwrap_or(true)
         }
         _ => false,
     };
