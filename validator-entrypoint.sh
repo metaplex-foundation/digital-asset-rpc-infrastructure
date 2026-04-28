d471f527abecee6810be3be1c470d37a47096922
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2026-04-28 10:58:16 -0700

    Update plerkle validator image in docker build

--- validator-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-apt-get update -qq && apt-get install -y -qq wget > /dev/null 2>&1
-wget -q http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
-dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb > /dev/null 2>&1
-rm -f libssl1.1_1.1.1f-1ubuntu2_amd64.deb
-
-exec ./runs.sh
