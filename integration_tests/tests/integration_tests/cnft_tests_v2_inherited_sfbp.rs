7062087c0ed5de3b65539fe2a228e98bea0a91c0
MarkSackerberg <93528482+MarkSackerberg@users.noreply.github.com>
2026-07-02 14:36:48 +0200

    fmt

--- integration_tests/tests/integration_tests/cnft_tests_v2_inherited_sfbp.rs
+++ integration_tests/tests/integration_tests/cnft_tests_v2_inherited_sfbp.rs
@@ -39,7 +39,10 @@ pub async fn run_inherited_sfbp_scenario_test(
 
         let response = setup.das_api.get_asset(request).await.unwrap();
 
-        let royalty = response.royalty.as_ref().expect("royalty should be present");
+        let royalty = response
+            .royalty
+            .as_ref()
+            .expect("royalty should be present");
         assert_eq!(royalty.royalty_model, RoyaltyModel::Creators);
         assert_eq!(royalty.target, None);
         assert_eq!(royalty.basis_points, expected_basis_points);
@@ -49,7 +52,10 @@ pub async fn run_inherited_sfbp_scenario_test(
         );
         assert_eq!(royalty.sfbp_inherited, Some(true));
 
-        let creators = response.creators.as_ref().expect("creators should be present");
+        let creators = response
+            .creators
+            .as_ref()
+            .expect("creators should be present");
         assert_eq!(creators.len(), 1);
         assert_eq!(creators[0].address, expected_collection_creator);
         assert_eq!(creators[0].share, 100);
@@ -75,13 +81,15 @@ async fn test_mint_v2_inherited_sfbp_resolves_collection_royalty_and_creators() 
     let asset_id = "BzQPqccY1c88XeVfvvqrJsryHYMcPuxKiCVo4R6MDznv";
     let collection = "HgLnYtaZ9Nd1PqoGXh9rPXSMo6xUpUd6D5tsV8RUs7Qf";
     let collection_creator = "CJkzXwVwqiaSvMuRb3obrZHdrPFjCMBJBDrjspn72tDv";
-    let tree_sig = "5aFu7ARYYaLXRLQAQXMfpxK26oeqA3LzuxjPJ7rXNdsXn9kySgLSQYxvpPHxje8n4yGrWjMw4Z4jjyr39W2NyEB7";
-    let mint_sig = "4CS8k5FDLm8p1EYWyhF16nZXdG4dFmDjUah3SzSxU4QLpbthivNT26dwiAzPoXJPi3jEmXobciDpd1G71yTWci4F";
+    let tree_sig =
+        "5aFu7ARYYaLXRLQAQXMfpxK26oeqA3LzuxjPJ7rXNdsXn9kySgLSQYxvpPHxje8n4yGrWjMw4Z4jjyr39W2NyEB7";
+    let mint_sig =
+        "4CS8k5FDLm8p1EYWyhF16nZXdG4dFmDjUah3SzSxU4QLpbthivNT26dwiAzPoXJPi3jEmXobciDpd1G71yTWci4F";
 
     let seeds: Vec<SeedEvent> = vec![
-        seed_txn(&tree_sig),
-        seed_account(&collection),
-        seed_txn(&mint_sig),
+        seed_txn(tree_sig),
+        seed_account(collection),
+        seed_txn(mint_sig),
     ];
 
     run_inherited_sfbp_scenario_test(
